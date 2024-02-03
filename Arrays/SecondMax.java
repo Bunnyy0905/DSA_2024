9a211759c9099a8b202d305ef39928b116659bd5
Ash <126411621+Bunnyy0905@users.noreply.github.com>
2024-02-03 23:49:22 +0530

    Update SecondMax.java

--- Arrays/SecondMax.java
+++ Arrays/SecondMax.java
@@ -5,14 +5,17 @@ import java.util.*;
 public class SecondMax
 {
 	public static void main (String[] args)
-	{	
+	{			
+		System.out.println("Enter the number of elements you want to put in the array: ");
 		Scanner read = new Scanner(System.in);
-		    int[] num = new int[3];
-		    for(int j=0; j<3; j++){
+		int n = read.nextInt();
+		System.out.println("Enter the elements of the array: ");
+		    int[] num = new int[n];
+		    for(int j=0; j<n; j++){
 		        num[j] = read.nextInt();
 		    }
 		Arrays.sort(num);   //Sorting the array in Ascending Order
-		System.out.println(num[1]); 
+		System.out.println("The second highest number is: "+num[1]); 
 
 		
 	
